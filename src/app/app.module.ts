7360804c9f2a2c6029cfcc697fd644a56511ec63
Nathaniel Adam <grizmoore95@gmail.com>
2018-03-08 15:49:38 -0500

    about to restore header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -82,6 +82,7 @@ import { ChartsModule } from 'ng2-charts/ng2-charts';
     ...APP_COMPONENTS,
     ...APP_DIRECTIVES,
     AppLoginRegisterComponent
+    AppHeaderComponent
   ],
   providers: [{
     provide: LocationStrategy,
