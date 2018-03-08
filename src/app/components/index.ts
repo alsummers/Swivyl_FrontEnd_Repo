153a6559ee7623291695d17dd153440e83af2523
grizmoore <34774952+grizmoore@users.noreply.github.com>
2018-03-08 10:32:03 -0500

    Revert "Gutting sections"

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,7 +1,7 @@
 export * from './app-aside';
 export * from './app-breadcrumbs';
 export * from './app-footer';
-
+export * from './app-header';
 export * from './app-sidebar';
 export * from './app-sidebar-footer';
 export * from './app-sidebar-form';
