121c5afe04a423a78f2c4c435c339064f105c50a
Allison Summers <allisonsummers44@gmail.com>
2018-03-12 13:47:05 -0400

    profile styling

---
+++ src/app/views/profile/company-profile/company-profile/company-profile.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './company-profile.component.html',
+  
+})
+export class CompanyProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
