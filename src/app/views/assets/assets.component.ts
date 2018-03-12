db21822a0caa12ba15acbf2c33a22cf57e86d915
Nathaniel Adam <grizmoore95@gmail.com>
2018-03-12 16:07:53 -0400

    Created reusable components

--- src/app/views/assets/assets.component.ts
+++ src/app/views/assets/assets.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AssetsService } from '../../Services/assets.service'
 import { InsuredPropertyComponent } from './insured-property/insured-property.component'
+import { InsuredFleetComponent } from './insured-fleet/insured-fleet.component'
 @Component({
   templateUrl: 'assets.component.html',
   styleUrls: ['./assets.component.scss']
