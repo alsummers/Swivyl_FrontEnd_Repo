e44688e78eea83bd9e1f6c73acbfb5e92760ef88
Clayton Campbell <breezeplz@gmail.com>
2018-03-12 14:10:14 -0400

    fixed sidebar w/header

--- src/app/components/app-breadcrumbs/app-breadcrumbs.component.ts
+++ src/app/components/app-breadcrumbs/app-breadcrumbs.component.ts
@@ -13,34 +13,34 @@ import 'rxjs/add/operator/filter';
 })
 
 export class AppBreadcrumbsComponent {
-  // breadcrumbs: Array<Object>;
-  // constructor(
-  //   private router: Router,
-  //   private route: ActivatedRoute
-  // ) {
-  //   this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event) => {
-  //     // empty array is set to be filled with items, these or the home/dashboard items in the top-middle of the page
-  //     this.breadcrumbs = [];
+  breadcrumbs: Array<Object>;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event) => {
+      // empty array is set to be filled with items, these or the home/dashboard items in the top-middle of the page
+      this.breadcrumbs = [];
 
-  //     let currentRoute = this.route.root,
-  //     url = '';
-  //     do {
-  //       const childrenRoutes = currentRoute.children;
-  //       currentRoute = null;
-  //       // tslint:disable-next-line:no-shadowed-variable
-  //       childrenRoutes.forEach(route => {
-  //         if (route.outlet === 'primary') {
-  //           const routeSnapshot = route.snapshot;
-  //           url += '/' + routeSnapshot.url.map(segment => segment.path).join('/');
-  //           console.log(route.outlet)
-  //           this.breadcrumbs.push({
-  //             label: route.snapshot.data,
-  //             url:   url
-  //           });
-  //           currentRoute = route;
-  //         }
-  //       });
-  //     } while (currentRoute);
-  //   });
-  // }
+      let currentRoute = this.route.root,
+      url = '';
+      do {
+        const childrenRoutes = currentRoute.children;
+        currentRoute = null;
+        // tslint:disable-next-line:no-shadowed-variable
+        childrenRoutes.forEach(route => {
+          if (route.outlet === 'primary') {
+            const routeSnapshot = route.snapshot;
+            url += '/' + routeSnapshot.url.map(segment => segment.path).join('/');
+            console.log(route.outlet)
+            this.breadcrumbs.push({
+              label: route.snapshot.data,
+              url:   url
+            });
+            currentRoute = route;
+          }
+        });
+      } while (currentRoute);
+    });
+  }
 }
