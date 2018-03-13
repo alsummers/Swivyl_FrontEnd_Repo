51a2f3f5df8222a38af30934e910e9c4592bbacc
Allison Summers <allisonsummers44@gmail.com>
2018-03-13 10:02:10 -0400

    added company-entity route

--- src/app/views/profile/profile.routes.ts
+++ src/app/views/profile/profile.routes.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
         ]
       },
       {
-        path: '/company-entity',
+        path: 'company-entity',
         component: CompanyProfileEntityComponent
       },
       {
