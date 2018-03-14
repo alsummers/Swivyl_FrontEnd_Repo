f5c39c8be08a977ff6d4ccf8aa6e1f6554c912a8
Clayton Campbell <breezeplz@gmail.com>
2018-03-14 15:45:26 -0400

    component editing

--- src/app/components/app-logout/app-logout.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-app-logout',
-  templateUrl: './app-logout.component.html',
-  styleUrls: ['./app-logout.component.scss']
-})
-export class AppLogoutComponent { }
--- src/app/views/dashboard/dashboard-quickconnect.component.ts
+++ src/app/views/dashboard/dashboard-quickconnect.component.ts
@@ -5,14 +5,14 @@ import { Router } from '@angular/router';
   selector: 'app-quick-connect',
   template: `
   <div class="col">
-  <div class="card">
+    <div class="card">
       <div class="card-body">
         <h4 class="card-title">Quick Connect</h4>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <button class="btn btn-dark">Save</button>
       </div>
     </div>
-</div>
+  </div>
   `
 })
 export class DashboardQuickConnectComponent implements OnInit {
