e6ceedb7995ec317bbdf15babf75f665353de55c
Allison Summers <allisonsummers44@gmail.com>
2018-03-19 08:48:59 -0400

    added edit modal to entities

--- src/app/views/profile/company-profile/company-profile-entity/company-entity-name.component.ts
+++ src/app/views/profile/company-profile/company-profile-entity/company-entity-name.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgModel } from '@angular/forms'
+import { NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import { EntityService } from '../../../../Services/entity.service';
 import { CompanyService } from '../../../../Services/company.service';
 import { AuthService } from '../../../../Services/auth.service';
@@ -23,7 +24,7 @@ import { AuthService } from '../../../../Services/auth.service';
 
   <div class="col">{{entity.entity_name}}</div>
   
-  <div class="col-1"><i class="fa fa-pencil" (click)="updateEntity(entityInfo)"></i></div>
+  <div class="col-1"><i class="fa fa-pencil" (click)="open(content)"></i></div>
   </div>
 
   </div>
@@ -40,13 +41,30 @@ import { AuthService } from '../../../../Services/auth.service';
   <div class="row justify-content-end">
     <button (click)="grabAllCompanyEntities()" class="btn">Next</button>
   </div>
-  </form>`,
+  </form>
+  <ng-template #content let-c="close" let-d="dismiss" >
+  <div class="modal-header">
+    <h4 class="modal-title">EDIT ENTITY</h4>
+    <button type="button" class="close" aria-label="Close" (click)="d('Cross click')">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="modal-body">
+    <input class="form-control">
+    {{entity}}
+  </div>
+  <div class="modal-footer">
+    <button type="button" class="btn btn-outline-dark" (click)="updateEntity($event)">Edit</button>
+  </div>
+</ng-template>
+  `,
   
 })
 export class CompanyEntityNameComponent implements OnInit {
   companyId: number
   enteties: object
-  constructor( private _entityService:EntityService, private _companyService: CompanyService, private _auth: AuthService ) { }
+  closeResult: string
+  constructor( private _entityService:EntityService, private _companyService: CompanyService, private _auth: AuthService, private modalService: NgbModal ) { }
 
   ngOnInit() {
     this._companyService.fetchcompany().subscribe(e => {
@@ -82,12 +100,36 @@ export class CompanyEntityNameComponent implements OnInit {
       this.enteties = e
     })
   }
+  updateEntity(e) {
+    this._entityService.updateEntity(e.target.id).subscribe ( e => 
+    console.log("hello")
+    )
+    
+  }
   removeEntity(e){
     this._entityService.deleteEntity(e.target.id).subscribe(e => {
       this.grabAllCompanyEntities()
     })
     
   }
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+  
 
   
   
