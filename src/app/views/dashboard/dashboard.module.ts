33e23febd34748e180521303286273af38282f14
Courtney Corlew <courtneycorlew@Courtneys-MacBook-Pro.local>
2018-03-19 13:56:48 -0400

    added to-do component

--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -3,20 +3,36 @@ import { FormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import {CommonModule} from '@angular/common';
+import { SlicePipe } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardActivityComponent, DashboardQuickConnectComponent } from './dashboard-activity.module'
 import { DashboardToDoComponent } from './dashboard-to-do.component'
 
+
+import {ToDoService} from '../../Services/to-do.service';
+import { CompanyService } from '../../Services/company.service';
+import { AuthService } from '../../Services/auth.service';
+
+
 @NgModule({
   imports: [
+    CommonModule,
     FormsModule,
     DashboardRoutingModule,
     ChartsModule,
     BsDropdownModule,
-    ButtonsModule.forRoot()
+    ButtonsModule.forRoot(),
+    NgbModule.forRoot()
   ],
-  declarations: [ DashboardComponent, DashboardActivityComponent, DashboardQuickConnectComponent, DashboardToDoComponent]
+  declarations: [ DashboardComponent, DashboardActivityComponent, DashboardQuickConnectComponent, DashboardToDoComponent],
+  providers: [
+    AuthService,
+    ToDoService,
+    CompanyService
+ 
+  ]
 })
 export class DashboardModule { }
