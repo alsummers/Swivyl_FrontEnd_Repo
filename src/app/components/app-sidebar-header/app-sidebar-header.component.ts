0cf979b5a92598115cc13a99d4d2d44cd64f5b64
Nathaniel Adam <grizmoore95@gmail.com>
2018-03-22 16:04:19 -0400

    Staging changes to make changes

--- src/app/components/app-sidebar-header/app-sidebar-header.component.ts
+++ src/app/components/app-sidebar-header/app-sidebar-header.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar-header',
   templateUrl: './app-sidebar-header.component.html'
 })
-export class AppSidebarHeaderComponent { }
+export class AppSidebarHeaderComponent {
+  companyImage:string
+  constructor(){}
+  // https://www.communitylandtrust.ca/wp-content/uploads/2015/10/placeholder.png
+  NgOnInit(){
+  this.companyImage ='http://localhost:3000/api/company/profile/'+localStorage.getItem('img')
+}
+ }
