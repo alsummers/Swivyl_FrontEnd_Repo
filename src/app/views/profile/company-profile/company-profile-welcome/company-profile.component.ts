6f7dc2baed87b266d80a2839561cf184d950e5ce
Allison Summers <allisonsummers44@gmail.com>
2018-03-26 15:09:32 -0400

    local sign in fix

--- src/app/views/profile/company-profile/company-profile-welcome/company-profile.component.ts
+++ src/app/views/profile/company-profile/company-profile-welcome/company-profile.component.ts
@@ -6,23 +6,27 @@ import { ActivatedRoute } from '@angular/router';
   
 })
 export class CompanyWelcomeProfileComponent implements OnInit {
-token: string;
-lastname: string;
-firstname: string;
-  constructor(private route: ActivatedRoute) { 
-    this.route.queryParams.subscribe(params => {
-      this.token = params['token'];
-      this.firstname = params['firstname'];
-      this.lastname = params['lastname'];
-  });
-  }
 
-  ngOnInit() {
+  constructor(){}
 
-    localStorage.setItem('token', this.token)
-    localStorage.setItem('firstName', this.firstname)
-    localStorage.setItem('lastName', this.lastname)
+  ngOnInit() {}
+// token: string;
+// lastname: string;
+// firstname: string;
+//   constructor(private route: ActivatedRoute) { 
+//     this.route.queryParams.subscribe(params => {
+//       this.token = params['token'];
+//       this.firstname = params['firstname'];
+//       this.lastname = params['lastname'];
+//   });
+//   }
 
-  }
+//   ngOnInit() {
+
+//     localStorage.setItem('token', this.token)
+//     localStorage.setItem('firstName', this.firstname)
+//     localStorage.setItem('lastName', this.lastname)
+
+//   }
 
 }
