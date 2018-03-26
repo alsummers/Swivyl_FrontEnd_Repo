1f6681bb8a8cead09f6f6b7e9e125cc26e25c63f
Allison Summers <allisonsummers44@gmail.com>
2018-03-26 14:35:56 -0400

    fixed google token

--- src/app/views/profile/company-profile/company-profile-welcome/company-profile.component.ts
+++ src/app/views/profile/company-profile/company-profile-welcome/company-profile.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './company-profile.component.html',
   
 })
 export class CompanyWelcomeProfileComponent implements OnInit {
-
-  constructor() { }
+token: string;
+lastname: string;
+firstname: string;
+  constructor(private route: ActivatedRoute) { 
+    this.route.queryParams.subscribe(params => {
+      this.token = params['token'];
+      this.firstname = params['firstname'];
+      this.lastname = params['lastname'];
+  });
+  }
 
   ngOnInit() {
+
+    localStorage.setItem('token', this.token)
+    localStorage.setItem('firstName', this.firstname)
+    localStorage.setItem('lastName', this.lastname)
+
   }
 
 }
