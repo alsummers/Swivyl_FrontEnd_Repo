dde4cb63019d02bfcb48737484375ad993e8e7a2
Courtney Corlew <courtneycorlew@Courtneys-MacBook-Pro.local>
2018-03-26 16:59:42 -0400

    added dropdown

--- src/app/views/profile/activity-connect.module.ts
+++ src/app/views/profile/activity-connect.module.ts
@@ -98,29 +98,71 @@ interface Client {
 @Component({
   selector: 'app-quick-connect',
   template: `
-  <div class="card dashboard-card quick-connect">
-  <div class="card-header to-do-header dashboard-header">
-    <p class="card-title">QUICK CONNECT</p>
-  </div>
-  <div class="card-body connect-box">
-    <style>::placeholder {
-      color: blue;
-      opacity: 0.35;
-      font-weight: bold;
-  }</style>
-      <textarea class="connect-textbox" rows="8" cols="75" placeholder="Need to contact your broker?&#10;Enter your message here."></textarea>
-  </div>
-  <div class="card-footer dashboard-footer">
-  <button id="submit" class="btn btn-dark">Submit</button>
-  </div>
-</div>
+    <div class="card dashboard-card quick-connect">
+      <div class="card-header to-do-header dashboard-header">
+        <p class="card-title">QUICK CONNECT</p>
+      </div>
+      <div class="card-body connect-box">
+    
+        <select (change)="onChange($event.target.value)" style="width: 100% !important;">
+          <option value='default' > Select</option>
+          <option *ngFor="let option of options" value="{{option.id}}"> {{option.title}} </option>
+        </select>
+        <div *ngIf="!currentId || currentId === 'default'">
+          <textarea class="connect-textbox" rows="8" cols="75" placeholder="Please select whom to contact from above" disabled></textarea>
+        </div>
+        <div *ngFor="let description of descriptions">  
+            <textarea class="connect-textbox" rows="8" cols="75" placeholder={{description.description}} *ngIf="currentId === description.id"></textarea>
+        </div>
+      </div>
+
+      <div class="card-footer dashboard-footer">
+      <button id="submit" class="btn btn-dark">Submit</button>
+      </div>
+    </div>
   `
 })
 export class DashboardQuickConnectComponent implements OnInit {
+currentId: any
+options: Object[] = [
+  {
+    "id": 1,
+    "title": 'broker'
+    },
+  {
+    "id": 2,
+    "title": 'accountant'
+  },
+  {
+    "id": 3,
+    "title": 'HR'
+  }
+]
+descriptions: Object[] = [
+
+  {
+    "id": '1',
+    "description": 'Please type all broker related questions here.'
+  },
+  {
+    "id": '2',
+    "description": 'Please type all accountant related questions here.'
+  },
+  {
+    "id": '3',
+    "description": 'Please type all HR related questions here.'
+  }
+]
 
 
-  ngOnInit() {}
-  
+  ngOnInit() {
+    
+   }
 
-  
+
+  onChange(e) {
+    this.currentId = e
+    console.log(this.currentId)
+  }
+
 }
