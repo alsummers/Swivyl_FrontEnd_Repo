344235ff1fe2cc4235e6f5022dd65a71a3c9252a
Nathaniel Adam <grizmoore95@gmail.com>
2018-03-26 08:53:19 -0400

    Image upload refreshes the page

--- src/app/Services/company.service.ts
+++ src/app/Services/company.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Headers, RequestOptions } from '@angular/http';
 import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http'
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable'
@@ -8,18 +8,30 @@ const Sql_Api = "http://localhost:3000/api/company"
 @Injectable()
 export class CompanyService {
 
-  constructor(private _http: HttpClient, private _router:Router) { }
+  constructor(private _http: HttpClient, private _router:Router) {
+    
+
+   }
   getHeaders(): HttpHeaders {
     return new HttpHeaders().set( 'Authorization', localStorage.getItem('token') )
   }
+  
   // registerCompany(companyInfo){
   //   return this._http.post(`${Sql_Api}/company/`, companyInfo ,{headers:this.getHeaders()})
   // }
   fetchcompany(){
     return this._http.get(`${Sql_Api}`, {headers:this.getHeaders()})
   }
-  updateCompany(){
-    return this._http.put(`${Sql_Api}`, {headers:this.getHeaders()} )
+  updateCompany(image){
+    
+    return this._http
+      .put(
+        `${Sql_Api}/image`, image , {
+          headers: new HttpHeaders({
+            'Authorization': localStorage.getItem('token'),
+            'Key':'companyLogo'
+          })
+        })
   }
 
   deleteCompany(){
--- src/app/components/app-sidebar-header/app-sidebar-header.component.ts
+++ src/app/components/app-sidebar-header/app-sidebar-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit} from '@angular/core';
+import { CompanyService } from '../../Services/company.service';
+
 
 @Component({
   selector: 'app-sidebar-header',
@@ -8,12 +10,28 @@ export class AppSidebarHeaderComponent {
   companyImage:string
   firstName:string
   lastName:string
-  constructor(){}
+  constructor(private _companyService:CompanyService){}
   // https://www.communitylandtrust.ca/wp-content/uploads/2015/10/placeholder.png
   ngOnInit(){
-  this.companyImage ='http://localhost:3000/profile/'+localStorage.getItem('img')
-  this.firstName=localStorage.getItem('firstName')
-  this.lastName=localStorage.getItem('lastName')
-  console.log(this.firstName)
-}
+    this.companyImage ='http://localhost:3000/profile/'+localStorage.getItem('img')
+    this.firstName=localStorage.getItem('firstName')
+    this.lastName=localStorage.getItem('lastName')
+  }
+  ImageSubmitClick(){
+    let input = document.getElementById('ImageSelect')
+   console.log(input)
+   input.click()
+  }
+  ImageSubmit(e){
+    let formData= new FormData();
+    console.log('------',formData)
+    formData.append('companyLogo', e.target.files[0] , 'companyLogo.jpg')
+    console.log(formData)
+    location.reload()
+    this._companyService.updateCompany(formData).subscribe(e=>{
+      console.log(e)
+      
+    })
+  }
+  
  }
