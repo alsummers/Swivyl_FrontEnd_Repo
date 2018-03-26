36b9acb6e3afb5686b3f5f670ab977d4e7166d58
Nathaniel Adam <grizmoore95@gmail.com>
2018-03-26 13:55:11 -0400

    this should fix client side issues with img, may have to check server for other fixes

--- src/app/containers/full-layout/full-layout.component.ts
+++ src/app/containers/full-layout/full-layout.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CompanyService } from '../../Services/company.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './full-layout.component.html'
 })
 
-export class FullLayoutComponent { }
+export class FullLayoutComponent {
+  constructor(private _companyService: CompanyService){}
+  companyId:string
+  ngOnInit() {
+    this._companyService.fetchcompany().subscribe(e => {
+      console.log(e[0])
+      this.companyId = e[0].uid
+      localStorage.setItem('company', e[0].uid)
+      localStorage.setItem('img', e[0].img)
+      
+      // return console.log("Company",this.companyId)
+    })
+
+  }
+  
+ }
