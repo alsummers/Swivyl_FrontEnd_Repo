0ab08fccbca95773e575bfe2f9d3b740d5ec7f72
Courtney Corlew <courtneycorlew@Courtneys-MacBook-Pro.local>
2018-03-27 16:56:00 -0400

    added server

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http')
+const path = require('path');
+
+const api = 'https://swivylback.herokuapp.com/api'
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const port = 3005
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running!!!!'))
